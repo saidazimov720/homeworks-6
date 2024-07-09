c48a93b2601dc4e2a3bfff37ad792653adc6758f
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-09 11:43:31 +0500

    last corrections

--- data_saver/script.js
+++ data_saver/script.js
@@ -3,10 +3,10 @@ var data = prompt("Enter your data");
 var text = [];
 var num = [];
 
-if (!isNaN(data) && data.trim() !=="") {
+if (!isNaN(data) && data !=="") {
     num.push(Number(data));
     console.log("Number array:", num);
-} else if(isNaN(data) && data.trim() !=="") {
+} else if(isNaN(data) && data !=="") {
     text.push(data);
     console.log("text array:", text);
 } else{
